d375ec4409a3b897e454f6097b924d14bec05ed1
Caique Torres <caiquetorres.bastos@hotmail.com>
2023-08-17 11:33:14 -0300

    feat: implemented a way of getting the cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+mod cli;
+
+use cli::command_line_parser::CommandLineParser;
+
 fn main() {
-    println!("Hello World!");
+    let mut parser = CommandLineParser::new();
+
+    parser.add_option("-f", "--file");
+    parser.add_option("-v", "--verbose");
+    parser.add_option("-r", "--repl");
+
+    let args = std::env::args().collect::<Vec<String>>();
+    let options = parser.parse(&args);
+
+    if options.has("--verbose") {
+        println!("Verbose mode enabled.");
+    }
+
+    if options.has("--repl") {
+        repl();
+    } else if options.has("--compile") {
+        compile();
+    }
 }
+
+fn repl() {}
+
+fn compile() {}
