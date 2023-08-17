8c1c8eb9e19599b5b635bf1dc2be101aee96d615
Caique Torres <caiquetorres.bastos@hotmail.com>
2023-08-17 11:28:48 -0300

    feat(node.rs): implemented some traits to represent how syntax nodes will look like

---
+++ src/lang/node.rs
@@ -0,0 +1,38 @@
+use std::{collections::VecDeque, fmt::Display};
+
+use super::token::Token;
+
+pub trait Node: ToString {
+    fn as_node(&self) -> &dyn Node;
+
+    fn get_token(&self) -> &Token;
+
+    fn get_children(&self) -> Vec<&dyn Node>;
+}
+
+pub trait ExpressionNode: Node {}
+
+impl Display for dyn ExpressionNode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut queue: VecDeque<Box<&dyn Node>> = VecDeque::new();
+        let mut depth_queue: VecDeque<usize> = VecDeque::new();
+
+        depth_queue.push_back(0);
+        queue.push_back(Box::new(self.as_node()));
+
+        while !queue.is_empty() {
+            let node = queue.pop_back().unwrap();
+            let depth = depth_queue.pop_back().unwrap();
+
+            // TODO: We need to improve this visualization. A suggestion would be https://pt.wikipedia.org/wiki/Tree
+            write!(f, "{}{}\n", "  ".repeat(depth), node.to_string())?;
+
+            for child in node.get_children() {
+                queue.push_back(Box::new(child));
+                depth_queue.push_back(depth + 1);
+            }
+        }
+
+        Ok(())
+    }
+}
